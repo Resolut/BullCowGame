02ba6a94b2bf86929975c37e8c30e37e5c8bcd51
Dmitriy Samofalov <samdimus@gmail.com>
2019-04-17 17:01:33 +0300

    Добавлен код определения функций CheckGuessValidity и SubminValidGuess IsIsogram

--- BullCowGame/FBullCowGame.cpp
+++ BullCowGame/FBullCowGame.cpp
@@ -1,46 +1,101 @@
 #include "FBullCowGame.h"
+#include <iostream>
+#include <map>
+#define TMap std::map
 
-FBullCowGame::FBullCowGame() { Reset(); }
+FBullCowGame::FBullCowGame() { /*MyCurrentTry = 0; MyMaxTries = 0;*/ Reset(); }
 
-int32 FBullCowGame::GetMAxTries() const { return MyMaxTries; }
+int32 FBullCowGame::GetMaxTries() const { return MyMaxTries; }
 int32 FBullCowGame::GetCurrentTry() const { return MyCurrentTry; }
-
-bool FBullCowGame::IsGameWon() const
-{
-	return false;
-}
+int32 FBullCowGame::GetHiddenWordLength() const { return MyHiddenWord.length(); }
+bool FBullCowGame::IsGameWon() const { return bGameIsWon; }
 
 void FBullCowGame::Reset()
 {
-	constexpr int32 MAX_TRIES = 5;
+	constexpr int32 MAX_TRIES = 4;
 	MyMaxTries = MAX_TRIES;
-	
+
 	const FString HIDDEN_WORD = "planet";
-	FString MyHiddenWord = HIDDEN_WORD;
+	MyHiddenWord = HIDDEN_WORD;
 
 	MyCurrentTry = 1;
+	bGameIsWon = false;
 
 	return;
 }
 
-bool FBullCowGame::CheckGuessValidity(FString str)
+EGuessStatus FBullCowGame::CheckGuessValidity(FString Guess) const
 {
-	return false;
+	if (!IsIsogram(Guess))
+	{
+		return EGuessStatus::Not_Isogram;
+	}
+	else if (false)
+	{
+		return EGuessStatus::Not_LowerCase;
+	}
+	else if (Guess.length() != GetHiddenWordLength()) {
+		return EGuessStatus::Wrong_Length;
+	}
+	else 
+	{
+		return EGuessStatus::OK;
+	}
 }
 
 // receives a VALID guess, incriments turn, and returns count
-FBullCowCount FBullCowGame::SubmitGuess(FString)
+FBullCowCount FBullCowGame::SubmitValidGuess(FString Guess)
 {
 	MyCurrentTry++;
 	FBullCowCount BullCowCount;
+	int32 WordLength = GetHiddenWordLength(); // assuming the same length as guess
 	
-	int32 HiddenWordLength = MyHiddenWorld.length;
-	for (int32 i = 0; i < HiddenWordLength; i++)
+	for (int32 i = 0; i < WordLength; i++)
 	{
-		for (int32 j = 0; j < HiddenWordLength; j++)
+		for (int32 j = 0; j < WordLength; j++)
 		{
-
+			if (Guess[j] == MyHiddenWord[i])
+			{
+				if (i == j) 
+				{
+					BullCowCount.Bulls++;
+				}
+				else 
+				{
+					BullCowCount.Cows++;
+				}
+			}
 		}
 	}
+	if (BullCowCount.Bulls == WordLength) 
+	{
+		bGameIsWon = true;
+	}
+	else {
+		bGameIsWon = false;
+	}
+	
 	return BullCowCount;
+}
+
+bool FBullCowGame::IsIsogram(FString Guess) const
+{
+	if (Guess.length() <= 1) return true;
+	
+	TMap<char, bool> LetterSeen;
+	
+	for (auto Letter : Guess)
+	{
+		Letter = tolower(Letter);
+		if (LetterSeen[Letter]) {
+
+			return false;
+		}
+		else {
+
+			LetterSeen[Letter] = true;
+		}
+	}
+	
+	return true;
 }
