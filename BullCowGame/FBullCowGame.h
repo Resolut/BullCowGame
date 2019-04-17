2ddcdf4e63c69c88abe21a36e08b258d07dc5a6e
Dmitriy Samofalov <samdimus@gmail.com>
2019-04-17 16:58:32 +0300

    доблен класс перечисление EGuessStatus; добавлены функции GetHiddenWord, CheckGuessValidity добавлены приватные поля bGameIsWon, IsIsogram; публичная функция SubmitGuess переименована в  SubmitValidGuess

--- BullCowGame/FBullCowGame.h
+++ BullCowGame/FBullCowGame.h
@@ -8,24 +8,37 @@ struct FBullCowCount
 {
 	int32 Bulls = 0;
 	int32 Cows = 0;
+};
 
+enum class EGuessStatus
+{
+	Invalid,
+	OK,
+	Not_Isogram,
+	Wrong_Length,
+	Not_LowerCase
 };
 
 class FBullCowGame 
 {
 public:
-	FBullCowGame();
-	int32 GetMAxTries() const;
+	FBullCowGame(); // constructor
+
+	int32 GetMaxTries() const;
 	int32 GetCurrentTry() const;
+	int32 GetHiddenWordLength() const;
 	bool IsGameWon() const;
+	EGuessStatus CheckGuessValidity(FString) const;
 	
 	void Reset();
-	bool CheckGuessValidity(FString);
-	FBullCowCount SubmitGuess(FString);
+	FBullCowCount SubmitValidGuess(FString);
 
 
 private:
 	int32 MyCurrentTry;
 	int32 MyMaxTries;
-	FString MyHiddenWorld;
+	FString MyHiddenWord;
+	bool bGameIsWon;
+
+	bool IsIsogram(FString) const;
 };
